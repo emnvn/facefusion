0ac88c2a7c57093fd127146cbdd71e2d48026d35
Nghia Em <emnguyen@eyecast.com>
2024-08-06 20:19:18 +0700

    fix(text): show about

--- facefusion/uis/components/about.py
+++ facefusion/uis/components/about.py
@@ -11,11 +11,11 @@ def render() -> None:
 	global ABOUT_BUTTON
 	global DONATE_BUTTON
 
-	# ABOUT_BUTTON = gradio.Button(
-	# 	value = metadata.get('name') + ' ' + metadata.get('version'),
-	# 	variant = 'primary',
-	# 	link = metadata.get('url')
-	# )
+	ABOUT_BUTTON = gradio.Button(
+		value = metadata.get('name') + ' ' + metadata.get('version'),
+		variant = 'primary',
+		link = metadata.get('url')
+	)
 	# DONATE_BUTTON = gradio.Button(
 	# 	value = wording.get('uis.donate_button'),
 	# 	link = 'https://donate.facefusion.io',
